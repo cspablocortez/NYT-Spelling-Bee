d20036c329d01d3958ae70d3ddf6cdb0b88b4449
Pablo Cortez <cspablocortez@gmail.com>
2024-04-04 19:18:58 -0700

    Update spelling_bee.rb

--- lib/spelling_bee.rb
+++ lib/spelling_bee.rb
@@ -2,9 +2,12 @@ require 'nokogiri'
 require 'open-uri'
 
 module SpellingBee
+    FILE_PATH = 'puzzles'
 
     def self.generate_url(year, month, day)
-        base_url = 'https://nytimes.com'
+        base_url = 'https://www.nytimes.com'
         path = '/crosswords/spelling-bee-forum.html'
-        # TODO: GET URL BASED ON DATE
+        url = "#{base_url}/#{year}/#{sprintf('%02d', month)}/#{sprintf('%02d', day)}/#{path}"
+    end
+
 end
