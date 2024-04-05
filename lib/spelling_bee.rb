ca4c856308b268434c787b9bd33d46ec89905940
Pablo Cortez <cspablocortez@gmail.com>
2024-04-04 19:23:24 -0700

    Update spelling_bee.rb

--- lib/spelling_bee.rb
+++ lib/spelling_bee.rb
@@ -3,6 +3,7 @@ require 'open-uri'
 
 module SpellingBee
     FILE_PATH = 'puzzles'
+    USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'
 
     def self.generate_url(year, month, day)
         base_url = 'https://www.nytimes.com'
