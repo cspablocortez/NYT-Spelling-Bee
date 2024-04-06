fbf218450845671fdcdea5a560fc58f7fe17f57a
Pablo Cortez <cspablocortez@gmail.com>
2024-04-05 17:59:29 -0700

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
+require_relative 'lib/spelling_bee'
 
 get '/' do
-    erb :date
+    t = Time.new.getlocal('-07:00')
+    puts "Current time: #{t.ctime}"
+    url = SpellingBee.generate_url(params['year'], params['month'], params['day'])
+    puts "Generated URL: #{url}"
+
+    html = SpellingBee.download_puzzle_html(url)
+
+    @puzzle = SpellingBee.get_puzzle(html)
+    erb :puzzle
+end
+
+get '/puzzle/:year/:month/:day' do
+    "You reached #{params['year']}/#{params['month']}/#{params['day']}"
 end
